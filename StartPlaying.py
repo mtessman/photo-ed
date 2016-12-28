f4d8654287cf166362e638fbf6e6bf5df020f67e
mtessman <tessmanm@msoe.edu>
2016-12-28 16:22:09 -0600

    Formats quit button

--- StartPlaying.py
+++ StartPlaying.py
@@ -7,9 +7,9 @@ def main():
     window = tk.Canvas(master, width=photo.width(), height=photo.height() + 50)
     window.create_image(0,50, image=photo, anchor=tk.NW)
 
-    button1 = tk.Button(master, text="Quit", command=quit, anchor=tk.W)
-    button1.configure(width=10, activebackground="#33B5E5", relief=tk.FLAT)
-    window.create_window(10, 10, anchor=tk.NW, window=button1)
+    quitButton = tk.Button(master, text="Quit", command=quit, anchor=tk.W, )
+    quitButton.configure(width=5, activebackground="#aaaaaa", relief=tk.GROOVE)
+    window.create_window(10, 10, anchor=tk.NW, window=quitButton)
 
     window.pack()
 
