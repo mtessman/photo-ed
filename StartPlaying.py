9cc95f7c11b71492b5b6285c42a55958ceba1bd7
nirganzerix <nirganzerix@hotmail.com>
2016-12-28 16:07:25 -0600

    add  button

--- StartPlaying.py
+++ StartPlaying.py
@@ -2,10 +2,17 @@ import Tkinter as tk
 
 def main():
     master = tk.Tk()
+
     photo = tk.PhotoImage(file = 'bb8.gif')
-    window = tk.Canvas(master, width=photo.width(), height=photo.height())
-    window.create_image(photo.width()/2,photo.height()/2, image=photo)
+    window = tk.Canvas(master, width=photo.width(), height=photo.height() + 50)
+    window.create_image(0,50, image=photo, anchor=tk.NW)
+
+    button1 = tk.Button(master, text="Quit", command=quit, anchor=tk.W)
+    button1.configure(width=10, activebackground="#33B5E5", relief=tk.FLAT)
+    window.create_window(10, 10, anchor=tk.NW, window=button1)
+
     window.pack()
+
     tk.mainloop()
 
 if __name__ == '__main__':
