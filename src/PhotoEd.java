15d48486a3beff2950d1a65a50ea30e2da58f00c
mtessman <tessmanm@msoe.edu>
2016-12-28 14:43:16 -0600

    Create Main File

---
+++ src/PhotoEd.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class PhotoEd {
+	
+	public static void main( String args[] ){
+		System.out.println("Hello World");
+		JFrame frame = new JFrame("FrameDemo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+		//frame.pack();
+		frame.setVisible(true);
+		
+	}
+}
