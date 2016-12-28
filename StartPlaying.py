90ac589d1c45138cfd39a6bc56f83c31a74b6d19
nirganzerix <nirganzerix@hotmail.com>
2016-12-28 16:34:51 -0600

    add toolbar menu

--- StartPlaying.py
+++ StartPlaying.py
@@ -1,16 +1,26 @@
 import Tkinter as tk
 
+def doNothing():
+    return 0
+
 def main():
     master = tk.Tk()
-
     photo = tk.PhotoImage(file = 'bb8.gif')
-    window = tk.Canvas(master, width=photo.width(), height=photo.height() + 50)
+    window = tk.Canvas(master, width=photo.width(), height=photo.height()+50)
     window.create_image(0,50, image=photo, anchor=tk.NW)
 
-    button1 = tk.Button(master, text="Quit", command=quit, anchor=tk.W)
-    button1.configure(width=10, activebackground="#33B5E5", relief=tk.FLAT)
-    window.create_window(10, 10, anchor=tk.NW, window=button1)
+    menu = tk.Menu(master)
+    fileMenu = tk.Menu(menu, tearoff=0)
+    fileMenu.add_command(label="Open", command=doNothing)
+    fileMenu.add_command(label="Save", command=doNothing)
+    fileMenu.add_command(label="Quit", command=quit)
 
+    menu.add_cascade(label='File', menu=fileMenu)
+
+    button1 = tk.Button(master, text="Quit", command=quit, anchor=tk.W)
+    button1.configure(width=5, background="#FFFFFF", relief=tk.FLAT)
+    window.create_window(10, 10, anchor=tk.NW, window=button1)
+    master.config(menu=menu)
     window.pack()
 
     tk.mainloop()
